37143bf784fe2b79173c70be2d5bf1f03c5633d2
leizton <leizton@126.com>
2024-09-16 12:43:19 +0800

    compile

--- run_build_blas.sh
+++ run_build_blas.sh
@@ -4,35 +4,28 @@ cd blas
 rm -rf lapack-3.12.0
 tar -zxf lapack-3.12.0.tar.gz
 cd lapack-3.12.0
-cp make.inc.example make.inc
-make -j 10 blaslib
-make -j 10 cblaslib
-make -j 10 lapacklib
-make -j 10 lapackelib
-if [ -f librefblas.a ]; then
+
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+  -DCMAKE_INSTALL_PREFIX=./build \
+  -B build .
+
+make -j 10 -C build
+make install -C build
+
+if [ -f ./build/lib64/libblas.a ]; then
   echo 'make succ: librefblas.a'
 else
   echo 'make fail: librefblas.a'
   exit 1
 fi
-if [ -f libcblas.a ]; then
-  echo 'make succ: libcblas.a'
-else
-  echo 'make fail: libcblas.a'
-  exit 1
-fi
-if [ -f liblapack.a ]; then
+
+if [ -f ./build/lib64/liblapack.a ]; then
   echo 'make succ: liblapack.a'
 else
   echo 'make fail: liblapack.a'
   exit 1
 fi
-if [ -f liblapacke.a ]; then
-  echo 'make succ: liblapacke.a'
-else
-  echo 'make fail: liblapacke.a'
-  exit 1
-fi
+
 cd ..
 cd ..
 exit 0
--- run_cmake.sh
+++ run_cmake.sh
@@ -12,10 +12,11 @@ echo '------------------------------'
 
 rm -rf build
 cmake -DBLAS_LIBRARIES=$lapack_dir -DLAPACK_LIBRARIES=$lapack_dir \
- -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF \
- -DFAISS_ENABLE_C_API=ON -DCMAKE_BUILD_TYPE=Release \
- -DFAISS_OPT_LEVEL=generic \
- -B build .
+  -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF \
+  -DFAISS_ENABLE_C_API=ON -DCMAKE_BUILD_TYPE=Release \
+  -DFAISS_OPT_LEVEL=generic \
+  -DCMAKE_INSTALL_PREFIX=$curr_dir/build \
+  -B build .
 if [ $? -ne 0 ]; then
   echo 'cmake build fail'
   exit 1
@@ -23,7 +24,12 @@ fi
 echo 'cmake build succ'
 echo '------------------------------'
 
-make -j 20 -C build
+make -j 10 -C build
 echo 'make succ'
 echo '------------------------------'
 
+make install -C build
+cp ./build/c_api/libfaiss_c.a build/lib64/
+cp blas/lapack-3.12.0/lib*.a build/lib64/
+echo 'install succ'
+echo '------------------------------'
